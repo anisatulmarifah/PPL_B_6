085e5ee4fc2518ddb0a2f66d64ba2d9764fcebcb
Hasna Paramesti Ahmad <hasnaparamesti@students.undip.ac.id>
2022-10-27 12:26:50 +0700

    update progress studi mhs

--- dosen/progress.php
+++ dosen/progress.php
@@ -114,35 +114,72 @@
   <div class="">
     <!-- Content -->
     <link rel="stylesheet" href="font.css" />
-    <h1 class="text-4xl font-medium mx-32 mb-2 my-20">Data Mahasiswa</h1>
-    <div class="w-96 justify-center flex items-center mx-auto space-x-1">
-      <a href="#" class="px-1 py-2 font-bold text-gray-500 hover:bg-blue-400 hover:text-white">
-          <
-      </a>
-      <a href="#" class="px-1 py-2 text-gray-700 hover:bg-blue-400 hover:text-white">
-          2022
-      </a>
-      <a href="#" class="px-1 py-2 text-gray-700 hover:bg-blue-400 hover:text-white">
-          2021
-      </a>
-      <a href="#" class="px-1 py-2 text-gray-700 hover:bg-blue-400 hover:text-white">
-          2020
-      </a>
-      <a href="#" class="px-1 py-2 text-gray-700 hover:bg-blue-400 hover:text-white">
-          2019
-      </a>
-      <a href="#" class="px-1 py-2 font-bold text-gray-500 hover:bg-blue-400 hover:text-white">
-          >
-      </a>
-  </div>
+    <h1 class="text-4xl mx-20 my-8 mt-24">Data Mahasiswa</h1>
+    <div class="flex justify-between mx-20 mt-8">
+      <!-- dropdown pilih tahun -->
+      <div>
+        <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" class="mb-4 center justify-center inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+          <svg class="w-4 h-4 text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
+          Pilih Tahun<svg class="ml-2 w-3 h-3" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+        </button>
+
+        <div id="dropdownRadio" class="hidden z-10 w-48 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top" style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);">
+                  <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioButton">
+                      <li>
+                          <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                              <input id="filter-radio-example-1" type="radio" value="" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                              <label for="filter-radio-example-1" class="ml-2 w-full text-sm font-medium text-gray-900 rounded dark:text-gray-300">2022</label>
+                          </div>
+                      </li>
+                      <li>
+                          <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                              <input checked="" id="filter-radio-example-2" type="radio" value="" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                              <label for="filter-radio-example-2" class="ml-2 w-full text-sm font-medium text-gray-900 rounded dark:text-gray-300">2021</label>
+                          </div>
+                      </li>
+                      <li>
+                          <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                              <input id="filter-radio-example-3" type="radio" value="" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                              <label for="filter-radio-example-3" class="ml-2 w-full text-sm font-medium text-gray-900 rounded dark:text-gray-300">2020</label>
+                          </div>
+                      </li>
+                      <li>
+                          <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                              <input id="filter-radio-example-4" type="radio" value="" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                              <label for="filter-radio-example-4" class="ml-2 w-full text-sm font-medium text-gray-900 rounded dark:text-gray-300">2019</label>
+                          </div>
+                      </li>
+                      <li>
+                          <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                              <input id="filter-radio-example-5" type="radio" value="" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                              <label for="filter-radio-example-5" class="ml-2 w-full text-sm font-medium text-gray-900 rounded dark:text-gray-300">2018</label>
+                          </div>
+                      </li>
+                  </ul>
+        </div>
+      </div>
+      <!-- end of dropdown -->
+
+      <!-- search bar -->
+      <div class="flex justify-right mb-4">
+      <label for="table-search" class="sr-only">Search</label>
+        <div class="relative">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="table-search" class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for mahasiswa">
+        </div>
+      </div>
+      <!-- end of search -->
+    
     </div>
 
-      <div class="p-5 col-span-7">
-        <div class="flex flex-col">
+      <!-- <div class="p-5 col-span-8"> -->
+        <!-- <div class="flex flex-col"> -->
           <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-              <div class="overflow-hidden">
-                <table class="min-w-full">
+              <div class="overflow-x-auto mx-20 relative shadow-md sm:rounded-lg">
+                <table id="list-mahasiswa" class="min-w-full">
                   <thead class="bg-white border-b">
                     <tr>
                       <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
@@ -169,7 +206,8 @@
                     </tr>
                   </thead>
                   <tbody>
-                    <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                    
+                    <!-- <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
                       <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
                       <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                         Anisatul Ma'rifah
@@ -272,7 +310,7 @@
                         <a href="progress_studi.php" class="font-medium text-green-600 dark:text-green-500 hover:underline">Lihat Progress</a>
                       </td>
                      </td>
-                    </tr>
+                    </tr> -->
                   </tbody>
                 </table>
               </div>
@@ -285,6 +323,31 @@
   </div>
 
   <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+  <script src="ajax.js"></script>
+  <script>
+    const angkatan = document.getElementsByName('filter-radio');
+    const filter2022 = angkatan[0];
+    const filter2021 = angkatan[1];
+    const filter2020 = angkatan[2];
+    const filter2019 = angkatan[3];
+    const filter2018 = angkatan[4];
+
+    filter2022.onchange = function() {
+      getAngkatan('2022', 'list-mahasiswa');
+    }
+    filter2021.onchange = function() {
+      getAngkatan('2021', 'list-mahasiswa');
+    }
+    filter2020.onchange = function() {
+      getAngkatan('2020', 'list-mahasiswa');
+    }
+    filter2019.onchange = function() {
+      getAngkatan('2019', 'list-mahasiswa');
+    }
+    filter2018.onchange = function() {
+      getAngkatan('2018', 'list-mahasiswa');
+    }
+  </script>
 </body>
 
 </html>
