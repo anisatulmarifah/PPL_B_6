72ff39fa264695a49a62a8092495058233920a88
Hasna Paramesti Ahmad <hasnaparamesti@students.undip.ac.id>
2022-10-27 13:39:24 +0700

    update

--- dosen/progress.php
+++ dosen/progress.php
@@ -198,9 +198,6 @@
                         Angkatan
                       </th>
                       <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                        Nilai
-                      </th>
-                      <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                         Action
                       </th>
                     </tr>
