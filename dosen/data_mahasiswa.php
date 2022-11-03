9c88ed68fbaf4b6cd9ab14f4a1a6018e4dfb5502
Aeoline <raubrey.kh@gmail.com>
2022-11-03 20:56:34 +0700

    bisa ambil session nimp

--- dosen/data_mahasiswa.php
+++ dosen/data_mahasiswa.php
@@ -2,7 +2,13 @@
 
 <?php
 session_start();
-require_once('../db_login.php')
+require_once('../db_login.php');
+$nip = $_SESSION['nip'];
+
+$query = "SELECT * FROM data_dosen WHERE nip = $nip";
+$result = $db->query($query);
+
+$dosen = $result->fetch_object();
 ?>
 <html>
 <link rel="stylesheet" href="style.css" />
@@ -132,9 +138,11 @@ require_once('../db_login.php')
 
 
           $sql = "SELECT * FROM data_mahasiswa as m, pkl, skripsi, verifikasi, data_dosen as d
-          WHERE m.doswal = d.nama
-          and 'nip' = d.nip
-          ";
+          WHERE m.doswal = d.nama_dosen
+          and $nip = d.nip
+          and m.nim = pkl.nim
+          and m.nim = skripsi.nim
+          and m.nim = verifikasi.nim";
           $result = $db->query($sql);
           $nomor = 0;
 
@@ -161,9 +169,7 @@ require_once('../db_login.php')
                   <td class='py-4 px-6 bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>".$row["status_skripsi"]. "</td>
                   <td class='py-4 px-6 bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>".$row["status_verifikasi"]. "</td>
                   <td class='py-4 px-6 space-x-4 bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
-                    <a href='#' type='button' data-modal-toggle='editModal' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit  </a>
                     <a href='#' type='button' data-modal-toggle='detailModal' class='font-medium text-green-600 dark:text-green-500 hover:underline'>Detail  </a>
-                    <button class='font-medium text-red-600 dark:text-red-500 hover:underline' type='button' data-modal-toggle='deleteModal'>Remove</button>
                   </td>
                 </tr>";
               }
@@ -340,6 +346,105 @@ require_once('../db_login.php')
     </div>
     <!-- End of Content -->
   </div>
+  <head>
+    
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--<title>Sidebar Menu | Side Navigation Bar</title>-->
+    <!-- CSS -->
+    
+    <!-- Boxicons CSS -->
+    <link
+      href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <nav>
+      <div class="logo">
+        <i class="bx bx-menu menu-icon"></i>
+        <span class="logo-name">Dashboard Dosen</span>
+      </div>
+      <div class="sidebar">
+        <div class="logo">
+          <i class="bx bx-menu menu-icon"></i>
+          <span class="logo-name">Universitas Diponegoro</span>
+        </div>
+
+        <div class="sidebar-content">
+          <ul class="lists">
+            <li class="list">
+              <a href="dashboard_dosen.php" class="nav-link">
+                <i class="bx bx-home-alt icon"></i>
+                <span class="link">Home</span>
+              </a>
+            </li>
+            
+            
+            <li class="list">
+              <a href="data_mahasiswa.php" class="nav-link">
+                <i class="bx bx-spreadsheet icon" ></i>
+                <span class="link">Data Mahasiswa </span>
+              </a>
+            </li>
+          
+            <div class="list"> 
+            <a href="verifikasi_data.php" class="nav-link"> 
+                <i class="bx bx-pie-chart-alt-2 icon"></i>
+                <span class="link">Verifikasi</span>  
+              </a>
+              </a>
+            </div>
+            <li class="list">
+              <a href="progress.php" class="nav-link">
+                <i class="bx bx-paste icon" ></i>
+                <span class="link">Progress Studi Mahasiswa</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="data_pkl.php" class="nav-link">
+                <i class="bx bx-folder-open icon"></i>
+                <span class="link">Data Mahasiswa PKL</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="data_skripsi.php" class="nav-link">
+                <i class="bx bx-folder-open icon"></i>
+                <span class="link">Data Mahasiswa Skripsi</span>
+              </a>
+            </li>
+          </ul>
+
+          <div class="bottom-cotent">
+            <li class="list">
+              <a href="../logout.php" class="nav-link">
+                <i class="bx bx-log-out icon"></i>
+                <span class="link">Logout</span>
+              </a>
+            </li>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="overlay"></section>
+
+    <script>
+      const navBar = document.querySelector("nav"),
+        menuBtns = document.querySelectorAll(".menu-icon"),
+        overlay = document.querySelector(".overlay");
+
+      menuBtns.forEach((menuBtn) => {
+        menuBtn.addEventListener("click", () => {
+          navBar.classList.toggle("open");
+        });
+      });
+
+      overlay.addEventListener("click", () => {
+        navBar.classList.remove("open");
+      });
+    </script>
 
   <head>
 </html>
