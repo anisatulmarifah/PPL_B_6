f96bd610aeeab7ae44dac409aa2562002d0f57b8
Hasna Paramesti Ahmad <hasnaparamesti@students.undip.ac.id>
2022-11-03 22:34:37 +0700

    Update dashboard_dosen.php

--- dosen/dashboard_dosen.php
+++ dosen/dashboard_dosen.php
@@ -42,7 +42,7 @@ $dosen = $result->fetch_object();
                 <label
                 class="block mb-2 mt-4 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >Nama Lengkap</label>
-                <input value="<?= $dosen->nama ?>" type="text" readonly class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                <input value="<?= $dosen->nama_dosen ?>" type="text" readonly class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
               </div>
               <div>
                 <label
